7635d310c5a33a5411a21946282c3ad8333c3814
Oleg Shvedov <www.goo5@me.com>
2016-10-25 17:03:33 +0300

    Solve problems in LinkedStack

--- seminar1/collections/LinkedStack.java
+++ seminar1/collections/LinkedStack.java
@@ -2,60 +2,93 @@ package seminar1.collections;
 
 import java.util.Iterator;
 
-public class LinkedStack<Item> implements IStack<Item> {
-
-    private Node<Item> head;
-    private int size;
+public class LinkedStack<E> implements IStack<E> {
+    private Node<E> head;
+    private int size = 0;
 
     @Override
-    public void push(Item item) {
-        /* TODO: implement it */
+    public void push(E item) {
+        Node<E> itemnode = new Node<>(item, null);
+        if (this.size == 0) {
+            this.head = itemnode;
+            this.size = 1;
+        } else {
+            Node<E> current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = itemnode;
+            this.size++;
+        }
     }
 
     @Override
-    public Item pop() {
-        /* TODO: implement it */
-        return null;
+    public E pop() {
+        if (this.size == 0) {
+            return null;
+        } else if (this.size == 1) {
+            E res = this.head.item;
+            this.head = null;
+            this.size--;
+            return res;
+        }
+
+        // Найти и взять значение удаляемого узла:
+        Node<E> current = this.head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        E res = current.item;
+
+        // Пройти еще раз для удаления узла (NPE при size=1):
+        Node<E> newcurrent = this.head;
+        while(newcurrent.next != current) {
+            newcurrent = newcurrent.next;
+        }
+        newcurrent.next = null;
+        this.size--;
+        return res;
     }
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return this.size == 0;
     }
 
     @Override
     public int size() {
-        return size;
+        return this.size;
     }
 
     @Override
-    public Iterator<Item> iterator() {
+    public Iterator<E> iterator() {
         return new LinkedStackIterator();
     }
 
-    private class LinkedStackIterator implements Iterator<Item> {
+    private class LinkedStackIterator implements Iterator<E> {
+        private Node<E> current = head;
 
         @Override
         public boolean hasNext() {
-            /* TODO: implement it */
-            return false;
+            return this.current != null;
         }
 
         @Override
-        public Item next() {
-            /* TODO: implement it */
-            return null;
+        public E next() {
+            E res = this.current.item;
+            this.current = this.current.next;
+            return res;
         }
-
     }
 
-    private static class Node<Item> {
-        Item item;
-        Node<Item> next;
+    private static class Node<E> {
+        E item;
+        Node<E> next;
 
-        public Node(Item item, Node<Item> next) {
+        public Node(E item, Node<E> next) {
             this.item = item;
             this.next = next;
         }
     }
+
 }
