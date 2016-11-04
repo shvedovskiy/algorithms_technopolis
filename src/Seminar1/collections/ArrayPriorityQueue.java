dc508468f6f3a15514b7db5710eb32b92767b3ab
Oleg Shvedov <www.goo5@me.com>
2016-11-05 01:03:15 +0300

    Implement binary heap data structure

--- src/Seminar1/collections/ArrayPriorityQueue.java
+++ src/Seminar1/collections/ArrayPriorityQueue.java
@@ -1,100 +1,146 @@
 package Seminar1.collections;
 
-import java.util.Comparator;
-import java.util.Iterator;
+import java.util.*;
 
 public class ArrayPriorityQueue<Key extends Comparable<Key>> implements IPriorityQueue<Key> {
-
-    private Key[] elementData;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Key[] elems;
     private Comparator<Key> comparator;
+    private int size;
 
+    @SuppressWarnings("unchecked")
     public ArrayPriorityQueue() {
-        /* TODO: implement it — O(n) */
+        elems = (Key[]) new Comparable[DEFAULT_CAPACITY];
     }
 
+    @SuppressWarnings("unchecked")
     public ArrayPriorityQueue(Comparator<Key> comparator) {
-        /* TODO: implement it — O(n) */
         this.comparator = comparator;
+        elems = (Key[]) new Comparable[DEFAULT_CAPACITY];
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayPriorityQueue(Key[] arr) {
+        elems = arr;
+        size = elems.length - 1;
+        for (int i = 0; i != size / 2; ++i) {
+            siftDown();
+        }
     }
 
     @Override
     public void add(Key key) {
-        /* TODO: implement it — O(log n) */
+        if (size >= elems.length - 1) {
+            grow();
+        }
+        size++;
+        int i = size;
+        elems[i] = key;
+        siftUp();
     }
 
     @Override
     public Key peek() {
-        /**
-         * TODO: implement it — O(1)
-         * Посмотреть на минимальный элемент
-         */
-        return null;
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+        return elems[1];
     }
 
     @Override
     public Key extractMin() {
-        /**
-         * TODO: implement it — O(log n)
-         * Достать минимальный элемент
-         *  и перестроить кучу
-         */
-        return null;
+        Key res = peek();
+        elems[1] = elems[size];
+        elems[size] = null;
+        size--;
+        siftDown();
+        if (size <= (elems.length >> 2)) {
+            shrink();
+        }
+        return res;
     }
 
     @Override
     public boolean isEmpty() {
-        /* TODO: implement it */
-        return false;
+        return size() == 0;
     }
 
     @Override
     public int size() {
-        /* TODO: implement it */
-        return 0;
+        return size;
     }
 
     private void siftUp() {
-        /**
-         * TODO: implement it — O(log n)
-         * Просеивание вверх —
-         *  подъём элемента больше родителей
-         */
+        int i = size;
+        while (i > 1 && greater(i / 2, i)) {
+            // while i has parent and parent greater than i
+            swap(i, i / 2);
+            i = i / 2;
+        }
     }
 
     private void siftDown() {
-        /**
-         * TODO: implement it — O(log n)
-         * Просеивание вниз
-         *  спуск элемента меньше детей
-         */
+        int i = 1;
+        while (i * 2 <= size) {
+            // while i has left child
+            int smallerChildIndex = i * 2;
+            if ((i * 2 + 1 <= size) && greater(i * 2, i * 2 + 1)) {
+                // i has right child and right child smaller than left child
+                smallerChildIndex = i * 2 + 1;
+            }
+            if (greater(i, smallerChildIndex)) {
+                swap(i, smallerChildIndex);
+            } else {
+                break;
+            }
+            i = smallerChildIndex;
+        }
     }
 
     private void grow() {
-        /**
-         * TODO: implement it
-         * Если массив заполнился,
-         * то увеличить его размер в полтора раз
-         */
+        int old_capacity = elems.length;
+        int new_capacity = old_capacity + (old_capacity >> 1);
+        changeCapacity(new_capacity);
     }
 
     private void shrink() {
-        /**
-         * TODO: implement it
-         * Если количество элементов в четыре раза меньше,
-         * то уменьшить его размер в два раза
-         */
+        int old_capacity = elems.length;
+        int new_capacity = old_capacity >> 1;
+        if (new_capacity >= DEFAULT_CAPACITY) {
+            changeCapacity(new_capacity);
+        }
+    }
+
+    private void changeCapacity(int newCapacity) {
+        elems = Arrays.copyOf(elems, newCapacity);
     }
 
     private boolean greater(int i, int j) {
         return comparator == null
-                ? elementData[i].compareTo(elementData[j]) > 0
-                : comparator.compare(elementData[i], elementData[j]) > 0
-                ;
+                ? elems[i].compareTo(elems[j]) > 0
+                : comparator.compare(elems[i], elems[j]) > 0;
+    }
+
+    private void swap(int i, int j) {
+        Key tmp = elems[i];
+        elems[i] = elems[j];
+        elems[j] = tmp;
     }
 
     @Override
     public Iterator<Key> iterator() {
-        /* TODO: implement it */
-        return null;
+        return new ArrayPriorityQueueIterator();
+    }
+
+    private class ArrayPriorityQueueIterator implements Iterator<Key> {
+        @Override
+        public boolean hasNext() {
+            return !isEmpty();
+        }
+
+        @Override
+        public Key next() {
+            return extractMin();
+        }
     }
 }
