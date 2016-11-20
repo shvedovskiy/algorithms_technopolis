c7da1ca24789515708c03f8396cfca64ee740def
Oleg Shvedov <www.goo5@me.com>
2016-11-21 02:29:36 +0300

    Implement iteration version

--- src/Lections/fourth/MergeSort.java
+++ src/Lections/fourth/MergeSort.java
@@ -52,3 +52,42 @@ public class MergeSort {
         }
     }
 }
+
+class IterableMergeSort {
+    private static void mergeSort(Comparable[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i *= 2) {
+            for (int j = 0; j < n - i; j += 2 * i) {
+                merge(arr, j, j + i, Math.min(j + 2 * i, n));
+            }
+        }
+    }
+
+    public static void merge(Comparable[] arr, int left, int mid, int right) {
+        Comparable[] tmp = new Comparable[right - left];
+        int it1 = 0;
+        int it2 = 0;
+
+        while ((left + it1 < mid) && (mid + it2 < right)) {
+            if (arr[left + it1].compareTo(arr[mid + it2]) <= 0) {
+                tmp[it1 + it2] = arr[left + it1];
+                it1++;
+            } else {
+                tmp[it1 + it2] = arr[mid + it2];
+                it2++;
+            }
+        }
+
+        while (left + it1 < mid) {
+            tmp[it1 + it2] = arr[left + it1];
+            it1++;
+        }
+
+        while (mid + it2 < right) {
+            tmp[it1 + it2] = arr[mid + it2];
+            it2++;
+        }
+
+        System.arraycopy(arr, 0, tmp, 0, it1 + it2);
+    }
+}
